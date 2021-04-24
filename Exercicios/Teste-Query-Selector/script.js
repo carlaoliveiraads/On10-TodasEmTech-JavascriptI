a08835f31141cd00c4382868604f44e359a33b37
Carla Oliveira <carlaoliveira.ads@gmail.com>
2021-04-23 21:44:18 -0300

    Progama rodando

---
+++ Exercicios/Teste-Query-Selector/script.js
@@ -0,0 +1,8 @@
+function mudarCor(){
+    const mudaCorTitulo = document.querySelector('.meu-titulo').style.color ="blue"
+
+    const mudaParagrafo = document.querySelector('#meuNome').innerHTML += "e eu tenho 30 anos."
+
+    const mudaSpan = document.querySelector('span').style.textDecoration = "underline"
+}
+
